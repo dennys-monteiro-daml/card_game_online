9d5e59c77058f51914043c1659fd273ad5f8f7cb
dennys-monteiro-daml <dennyshagamenom@gmail.com>
2022-09-08 00:31:51 -0300

    first update test
    Just testing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# card_game_online
+Projeto: Card Game Online
+ Objetivo: Jogo de cartas no melhor eestilo Munchikin onde é acessível jogar por qualquer dispositivo que acesse navegador e esteja conctado a internet.
+ Autor: Dennys Monteiro
+ Versão: 0.01
+ Início: 08/09/22
---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Projeto: Card Game Online
+ * Objetivo: Jogo de cartas no melhor eestilo Munchikin onde é acessível jogar por qualquer dispositivo que acesse navegador e esteja conctado a internet.
+ * Autor: Dennys Monteiro
+ * Versão: 0.01
+ * Início: 08/09/22
+ *  */
+
+const jogador = []
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
+rolard6();{
+  function rolard6() {
+  const d6 = [1, 2, 3, 4, 5, 6,], {
+    rolar = [], for: }; (let); i = 0; i < d6.length; i++; {
+    const rolar = d6[index];
+
+    }
+  }
+};
+
+this.element = document.getElementById('genero');
+this.element = ['Masculino', 'Feminino'];
